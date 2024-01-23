5a4e88c06fef3275f86f4624760e3d0dddababb9
liuzhehao123 <2963095858@qq.com>
2024-01-23 17:52:26 +0800

    几道输出题目

---
+++ 4.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%#o %#X", 1234, 1234); 
+    return 0;
+}
---
+++ 5.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 0XABCDEF;
+    printf("%15d\n", a);
+    return 0;
+}
