970b0df7a17bcc130309c888f9646dffc10a30c2
liuzhehao123 <2963095858@qq.com>
2024-02-01 23:23:25 +0800

    2024年2月1日

---
+++ NK/BC56.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+   unsigned int a;
+    while (~scanf("%d", &a))
+    {
+       while (a > 0)
+       {
+        printf("*");
+        a--;
+       }
+       printf("\n");
+    }
+
+    return 0;
+}
