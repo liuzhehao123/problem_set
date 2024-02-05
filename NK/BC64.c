27c093fded92e0b735199989f71289f5ac839a22
liuzhehao123 <2963095858@qq.com>
2024-02-05 21:26:11 +0800

    2024年2月5日

--- NK/BC64.c
+++ NK/BC64.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
-int main() {
+int main() 
+{
     int n = 0;
-    while (scanf("%d", &n) != EOF) {
+    while (scanf("%d", &n) != EOF) 
+    {
         //上一半
-       for (int i = n+1; i>0; i--) {
-            for (int j = i; j>0; j--) {
+       for (int i = n+1; i>0; i--) 
+       {
+            for (int j = i; j>0; j--) 
+            {
                 printf("* ");
             }
             printf("\n");
         }
         //下一半
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= i; j++) {
+        for (int i = 1; i <= n; i++) 
+        {
+            for (int j = 0; j <= i; j++) 
+            {
                  printf("* ");
             }
             printf("\n");
         }
     }
     return 0;
-}
+} 
 
