e1afb636b08d47f4acef92160a0ab96524b1333f
liuzhehao123 <lzh_daily@163.com>
2024-02-20 18:28:57 +0800

    2024年2月20日

--- test.cpp
+++ test.cpp
@@ -1,152 +1,151 @@
+#include <iostream>
+#include <vector>
 #include <algorithm>
-#include <cstdio>
-#define N 100010
 
 using namespace std;
 
-int a[N], c[N], n;
-int val[N], siz[N], maxn[N], fa[N], ch[N][2], len, root;
+struct Node {
+    int key; // ai值，代表任务的重要性
+    int height; // AVL树的高度
+    int count; // 保存该节点及其左子树包含的节点总数
+    Node* left;
+    Node* right;
+    Node(int k) : key(k), height(1), count(1), left(nullptr), right(nullptr) {}
+};
 
-int read()
-{
-    static int res;
-    static char op;
-    do
-    {
-        op = getchar();
-    } 
-    while (op > '9' || op < '0');
-    {
-        for (res = 0; op >= '0' && op <= '9'; op = getchar())
-        {
-            res = res * 10 + (op ^ 48);
-        }
-    }
-    return res;
+// 工具函数，获取节点的高度
+int height(Node* N) {
+    if (N == nullptr)
+        return 0;
+    return N->height;
 }
 
-bool relation(int x)
-{
-    return ch[fa[x]][1] == x;
+// 工具函数，获取节点的count
+int getCount(Node* N) {
+    if (N == nullptr)
+        return 0;
+    return N->count;
 }
 
-void update(int x)
-{
-    siz[x] = siz[ch[x][0]] + siz[ch[x][1]] + 1;
-    int p = a[val[x]], ch1 = a[maxn[ch[x][0]]], ch2 = a[maxn[ch[x][1]]];
-    if (p > ch1 && p > ch2)
-    {
-        maxn[x] = val[x];
-    }
-    else if (ch1 > ch2)
-    {
-        maxn[x] = maxn[ch[x][0]];
-    }
-    else
-        maxn[x] = maxn[ch[x][1]];
+// 用于更新节点N的高度和count
+void updateNode(Node* N) {
+    N->height = max(height(N->left), height(N->right)) + 1;
+    N->count = getCount(N->left) + getCount(N->right) + 1;
 }
 
-void rotate(int x)
-{
-    int f = fa[x], ff = fa[f], k = relation(x), y = ch[x][!k];
-    fa[f] = x;
-    fa[x] = ff;
-    fa[y] = f;
-    ch[x][!k] = f;
-    ch[f][k] = y;
-    ch[ff][ch[ff][1] == f] = x;
-    update(f);
-    update(x);
+// 右旋转
+Node* rightRotate(Node* y) {
+    Node* x = y->left;
+    Node* T2 = x->right;
+
+    // 旋转
+    x->right = y;
+    y->left = T2;
+
+    // 更新高度和count
+    updateNode(y);
+    updateNode(x);
+
+    return x;
 }
 
-void _splay(int x, int p)
-{
-    for (int f; fa[x] != p; rotate(x))
-    {
-        if (fa[f = fa[x]] != p)
-        {
-            rotate(relation(x) ^ relation(f) ? x : f);
-        }
-    }
+// 左旋转
+Node* leftRotate(Node* x) {
+    Node* y = x->right;
+    Node* T2 = y->left;
 
-    if (!p)
-    {
-        root = x;
-    }
+    // 旋转
+    y->left = x;
+    x->right = T2;
+
+    // 更新高度和count
+    updateNode(x);
+    updateNode(y);
+
+    return y;
 }
 
-int _find(int rank)
-{
-    int now = root;
-    while (siz[ch[now][0]] + 1 != rank)
-    {
-        if (siz[ch[now][0]] >= rank)
-        {
-            now = ch[now][0];
-        }
-        else
-            rank -= siz[ch[now][0]] + 1, now = ch[now][1];
-    }
-
-    return now;
+// 获取平衡因子
+int getBalance(Node* N) {
+    if (N == nullptr)
+        return 0;
+    return height(N->left) - height(N->right);
 }
 
-void ins(int &x, int f, int v)
-{
-    if (!x)
-    {
-        x = ++len;
-        fa[x] = f;
-        maxn[x] = val[x] = v;
-        siz[x] = 1;
+// 将key插入到AVL树中，并返回新树的根
+Node* insert(Node* node, int key, int* order, int position) {
+    if (node == nullptr) {
+        order[position] = key;
+        return new Node(key);
+    }
+
+    if (key < node->key)
+        node->left = insert(node->left, key, order, position);
+    else {
+        node->right = insert(node->right, key, order, position + getCount(node->left) + 1);
+    }
+
+    updateNode(node);
+
+    int balance = getBalance(node);
+
+    // 左左情况
+    if (balance > 1 && key < node->left->key)
+        return rightRotate(node);
+
+    // 右右情况
+    if (balance < -1 && key > node->right->key)
+        return leftRotate(node);
+
+    // 左右情况
+    if (balance > 1 && key > node->left->key) {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    // 右左情况
+    if (balance < -1 && key < node->right->key) {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    return node;
+}
+
+// 删除给定树的所有节点
+void deleteTree(Node* node) {
+    if (node == nullptr)
         return;
-    }
 
-    if (a[v] > a[maxn[ch[x][1]]] && a[v] > a[val[x]])
-    {
-        ins(ch[x][0], x, v);
-    }
-    else
-        ins(ch[x][1], x, v);
-    update(x);
+    deleteTree(node->left);
+    deleteTree(node->right);
+    delete node;
 }
 
-void print(int x)
-{
-    if (ch[x][0])
-    {
-        print(ch[x][0]);
+int main() {
+    int n;
+    cin >> n;
+    vector<pair<int, int>> people(n);
+    vector<int> order(n);
+    Node* root = nullptr;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> people[i].first >> people[i].second;
+    }
+  
+    // 根据重要性ai进行排序
+    sort(people.begin(), people.end());
+
+    for (int i = 0; i < n; ++i) {
+        // 在AVL树中插入，并更新队列中的排序
+        root = insert(root, i + 1, order.data(), people[i].second);
     }
 
-    printf("%d ", val[x]);
-
-    if (ch[x][1])
-    {
-        print(ch[x][1]);
+    for (int i : order) {
+        cout << i << " ";
     }
-}
+    cout << endl;
 
-signed main(int argc, char **argv)
-{
-    n = read();
-    for (int i = 1; i <= n; ++i)
-    {
-        a[i] = read(), c[i] = read();
-    }
-
-    for (int i = 1; i <= n; ++i)
-    {
-        if (i - c[i] > 1)
-        {
-            _splay(_find(i - c[i] - 1), 0);
-            ins(ch[root][1], root, i);
-        }
-        else
-            ins(root, 0, i);
-        _splay(len, 0);
-    }
-
-    print(root);
-
+    deleteTree(root); // 清理内存
     return 0;
 }
