b6969ecb35617bfed71a22521ac5ec83b96ad1e5
liuzhehao123 <lzh_daily@163.com>
2024-04-14 18:59:19 +0800

    第15届蓝桥杯真题C++ B组

---
+++ 20240414/1.cpp
@@ -0,0 +1,15 @@
+//握手问题 答案1204
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int cnt = 0;
+    for (int i = 7; i <= 49; ++i)
+        cnt += i;
+
+    cout << cnt << endl;
+
+    return 0;
+}
