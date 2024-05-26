7c2beda3dd04e0ae68c81000b79f9635424e40b5
liuzhehao123 <lzh_daily@163.com>
2024-05-26 20:35:40 +0800

    Div.4 937 A

--- Codeforces/Div4-937/A.cpp
+++ Codeforces/Div4-937/A.cpp
@@ -1,1 +1,30 @@
-//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (a < b && b < c)
+        {
+            cout << "STAIR" << endl;
+        }
+        else if (a < b && b > c)
+        {
+            cout << "PEAK" << endl;
+        }
+        else
+        {
+            cout << "NONE" << endl;
+        }
+    }
+
+    return 0;
+}
