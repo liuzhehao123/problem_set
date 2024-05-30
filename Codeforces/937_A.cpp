907fa74ea862250a624b3df70292b6573a267979
liuzhehao123 <lzh_daily@163.com>
2024-05-30 21:46:17 +0800

    Codeforces Round 937 (Div. 4)

---
+++ Codeforces/937_A.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a < b && b < c) 
+        cout << "STAIR" << endl;
+    else if (a < b && b > c) 
+        cout << "PEAK" << endl;
+    else 
+        cout << "NONE" << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
