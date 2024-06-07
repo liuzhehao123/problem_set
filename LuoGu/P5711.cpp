2cfd1048fb51cca29e71a9c3163df3ef109d74a6
liuzhehao123 <lzh_daily@163.com>
2024-06-07 23:06:35 +0800

    分支结构

---
+++ LuoGu/P5711.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    bool exist = 0;
+    if (n % 400 == 0 || (n % 100 != 0 && n % 4 == 0))
+        exist = 1;
+    
+    cout << exist << endl;
+
+    return 0;
+}
