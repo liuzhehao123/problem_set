075b51be834dc59d49198e60dd81ef4f7161fa65
liuzhehao123 <lzh_daily@163.com>
2024-06-20 16:09:21 +0800

    循环结构

--- LuoGu/P5721.cpp
+++ LuoGu/P5721.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-int main() {
+int main()
+{
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
     std::cout.tie(0);
@@ -9,12 +10,12 @@ int main() {
     std::cin >> n;
 
     int num = 0;
-    for (int i = 0; i <= n; ++i) {
-        for (int j = i; j < n; ++j) {
+    for (int i = 0; i <= n; ++i)
+    {
+        for (int j = i; j < n; ++j)
+        {
             num++;
-            if (num < 10) {
-                std::cout << 0;
-            }
+            if (num < 10) std::cout << 0;
             std::cout << num;
         }
         std::cout << '\n';
