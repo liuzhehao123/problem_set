00a36b2c59436fba02f216fe1f4c45ea162bc82a
liuzhehao123 <lzh_daily@163.com>
2024-06-21 12:15:14 +0800

    尝试

--- test.cpp
+++ test.cpp
@@ -1,25 +1,43 @@
 #include <iostream>
 
-using i64 = long long;
-
-void solve()
-{
-    
-}
-
 int main()
 {
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
     std::cout.tie(0);
 
-    int t;
-    std::cin >> t;
+    int k;
+    std::cin >> k;
 
-    while (t--)
+    int coin = 0, cnt = 1;
+    for (int i = 1; i <= k; ++i)
     {
-        solve();
+        k -= i;
+        coin = coin + cnt * cnt;
+        cnt++;
     }
 
+    std::cout << coin + k * cnt << \'n';
+    
     return 0;
 }
+
+/* #include <iostream>
+
+int main()
+{
+    int k;
+    std::cin >> k;
+
+    int coin = 0, cnt = 1; //coin是金币，cnt是比原来每天多获得的金币数
+    for (int i = 1; i <= k; ++i)
+    {
+        k -= i;
+        coin = coin + cnt * cnt;
+        cnt++;
+    }
+
+    std::cout << coin + k * cnt << '\n';
+
+    return 0;
+} */
