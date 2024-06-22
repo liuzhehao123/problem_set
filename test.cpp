2531495891b0d43b71bb67c022e4f631f59ae527
liuzhehao123 <lzh_daily@163.com>
2024-06-23 00:39:13 +0800

    刷题模板

--- test.cpp
+++ test.cpp
@@ -1,49 +1,10 @@
 #include <bits/stdc++.h>
 
-using ll = long long;
+typedef long long ll;
 
 void solve()
 {
-    int n;
-    std::cin >> n;
-    string s;
-    std::cin >> s;
     
-    for (int i = 1; i <= n; i++)
-    {
-        if (n % i == 0)
-        {
-            int satisfy = 2;
-            for (int j = 0; j < i; j++)
-            {
-                for (int k = j + i; k < n; k += i)
-                {
-                    if (s[k] != s[j]) satisfy--;
-                }
-            }
-
-            if (satisfy > 0)
-            {
-                std::cout << i << '\n';
-                return;
-            }
-
-            satisfy = 2;
-            for (int j = n - i; j < n; j++)
-            {
-                for (int k = j - i; k >= 0; k -= i)
-                {
-                    if (s[k] != s[j]) satisfy--;
-                }
-            }
-
-            if (satisfy > 0)
-            {
-                std::cout << i << '\n';
-                return;
-            }
-        }
-    }
 }
 
 int main()
