b3c0f9767b2ce3aedc44ab99764f5059cd237e8a
liuzhehao123 <lzh_daily@163.com>
2024-07-06 01:00:38 +0800

    刷题的模板

--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
 typedef long long ll;
-// using long long = ll;
+//using ll = long long;
 
 void solve() {
 
@@ -12,10 +12,10 @@ int main() {
     std::cin.tie(nullptr);
 
     int t;
-    std::cin >> t;
+    std:cin >> t;
     while (t--) {
         solve();
     }
-
+    
     return 0;
 }
