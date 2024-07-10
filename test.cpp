52569a8559f6330bdd4df0a7852ef49f9c94ac11
liuzhehao123 <lzh_daily@163.com>
2024-07-10 18:00:20 +0800

    刷题的固定模板

--- test.cpp
+++ test.cpp
@@ -1,19 +1,17 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
 using ll = long long;
 
 void solve() {
-
+    
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
 
     int t;
-    cin >> t;
+    std::cin >> t;
     while (t--) {
         solve();
     }
